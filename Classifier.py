c4377292841a685a0925aa62d93a9717df37a26c
lironsel <lironseliktar@gmail.com>
2017-06-19 00:51:38 +0300

    with main window almost finished

---
+++ Classifier.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+class Classifier:
+
+    def __init__(self):
+        self.path = None
+
+    def buildModel(self, path, bins):
+        return
+
+
+    def classifier(self):
+        target = "yes"
+        return target
+
+    def classify(self, path_test):
+        df_test = pd.read_csv(path_test)
+
+        list = ['yes', 'yes', 'no']
+        return list
+
+
+
--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 import Tkinter as tk
 import tkFileDialog
 from Tkinter import *
-
-#from Classifier import *
+import os.path
+import tkMessageBox
+from Classifier import *
 
 class Main(Frame):
-
+    # window design
     def showWindow(self):
         lbl_dir = Label(self, text="Directory Path: ")
         lbl_dir.grid(row=0, column=0, pady=40)
@@ -16,27 +17,70 @@ class Main(Frame):
         browse_button = Button(self, text="Browse", command=self.browse)
         browse_button.grid(row=0, column=4, sticky=E)
 
-        lbl_bins = Label(self, text="Number of Bins: ")
+        lbl_bins = Label(self, text="Discretization Bins: ")
         lbl_bins.grid(row=2, column=0)
 
         self.entry_bins = Entry(self)
         self.entry_bins.grid(row=2, column=2)
 
-        build_button = Button(self, text="Build", command=self.browse)
+        build_button = Button(self, text="Build", command=self.buildModel)
         build_button.grid(row=3, column=2, sticky=E, pady=15)
 
-        classify_button = Button(self, text="Build", command=self.browse)
+        classify_button = Button(self, text="Classify", command=self.classify)
         classify_button.grid(row=3, column=0, sticky=E, pady=15)
 
+    # constructor of main window
     def __init__(self, master=None):
         Frame.__init__(self, master)
+        self.classifier = Classifier()
+        self.isBuild = False
+        self.dir = None
+        self.bins = None
         self.pack()
         self.showWindow()
 
+    # show browser window and put path in needed entry
     def browse(self):
-        dir= tkFileDialog.askdirectory()
-        self.entry_dir.insert(0, dir)
+        self.dir = tkFileDialog.askdirectory()
+        self.entry_dir.insert(0, self.dir)
 
+    # check if a path to file is exist, return true or false accordingly and show error if needed
+    def checkPath(self, path):
+        if not os.path.exists(path):
+            tkMessageBox.showinfo("Error Message", "path: " + path + "doesn't exist")
+            return False
+        return True
+
+    # call the pre processing functions in the classifier, and chanfe current boolean to true
+    def buildModel(self):
+        self.bins = self.entry_bins.get()
+        path_train = self.dir + "\\train.csv"
+        if self.checkPath(path_train):
+            Classifier.buildModel(self.classifier, path_train,  self.bins)
+            self.isBuild = True
+            tkMessageBox.showinfo("Naive Bayes Classifier", "Building classifier using train-set is done!")
+
+    # write the results to output file, receive a list
+    def writeToFile(self, targets):
+        path_output = self.dir + "\\output.txt"
+        with open(path_output, "a") as f:
+            for i, v in enumerate(targets):
+               # print i+1, v
+                f.write(str(i+1) + " " + v + "\n")
+
+    # check if the file exist, and check pre processing happend already before writing results and finishing
+    def classify(self):
+        path_test = self.dir + "\\testtest.csv"
+        if self.checkPath(path_test):
+            if not self.isBuild:
+                tkMessageBox.showinfo("Naive Bayes Classifier", "You must Build before you can Classify!")
+            else:
+                if self.checkPath(path_test):
+                    targets = Classifier.classify(self.classifier, path_test)
+                    self.writeToFile(targets)
+                    root.withdraw()
+                    tkMessageBox.showinfo("Naive Bayes Classifier", "Classifying finished!")
+                    os._exit(0)
 
 root = tk.Tk()
 root.title('Naive Bayes Classifier')
