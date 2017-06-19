40961e58acf676ec14bc6148e6bd2f938b2be3f7
nitzan <nitzanmassad@gmail.com>
2017-06-19 11:54:04 +0300

    18.6.17

--- mainScript.py
+++ mainScript.py
@@ -3,9 +3,17 @@ import numpy as np
 import anaconda_mode as md
 #import tkinter as tk
 m_dictStructure = {}
-m_bins =0
+m_bins =10
+df =  file
+mainDic ={}
 
+def buildModel (path, bins):
 
+    m_bins = bins
+    readStructure(path)
+    fillMissingValuesAndDiscretization()
+    buildClassifier()
+    return
 
 def readStructure (pathToStructureInfoFile):
     file = open(pathToStructureInfoFile,"r")
@@ -19,10 +27,7 @@ def readStructure (pathToStructureInfoFile):
             tmp[2] = tmp[2].split(',')
 
         m_dictStructure[tmp [1]]= tmp [2]
-
     return
-
-
 
 def fillMissingValuesAndDiscretization():
     tmp = 'NUMERIC'
@@ -30,41 +35,44 @@ def fillMissingValuesAndDiscretization():
     for x in m_dictStructure:
         if(m_dictStructure[x]== 'NUMERIC'):
             df[x].fillna(df[x].mean(), inplace=True)
-            df[x] =binning(df["x"])
-            print df[x]
+            df[x] =binning(df[x])
         else:
             df[x].fillna(df.mode()[x][0], inplace=True) # find the most comman value in month
-
-
-
     #print(df.apply(lambda x: sum(x.isnull()), axis=0)) # print the missing values in eche colmn
-
     return
 
-def binning(col, cut_points ):
+def binning(col ):
     labels = None
     minval = col.min()
     maxval = col.max()
-    cut_points = maxval-minval
-    break_points = [minval] + cut_points + [maxval]
+    interval = (maxval-minval)*1.0/m_bins
+    #print "maxval: ",maxval
+    #print "minval: ",minval
+    #print "interval: ",interval
+    cut_points =[]
+    for x in range(0, m_bins+1):
+        cut_points.append((minval+ x*interval))
+    break_points = cut_points
     if not labels:
-        labels = range(len(cut_points)+1)
+        labels = range(len(cut_points)-1)
     colBin = pd.cut(col, bins=break_points, labels=labels, include_lowest=True)
     return colBin
+
+def buildClassifier () :
+
+    for attribut in m_dictStructure:
+
+
+        for
+
+
+   # print(df.loc[(df["education"] == "secondary") & (df["marital"] == "married"),["education", "marital"]])
+
+
+    return
+
 
 
 
 df = pd.read_csv("C:\Users\\nitzan\Desktop\\train.csv")
-
-#df['education'].fillna('No', inplace=True)
-#df['education'].fillna(df['education'].mean(), inplace=True)
-
-df1 = pd.DataFrame({'A': [1, 2, 1, 2, 1, 2, 3, 1] , 'B': [1, 2, 1, 2, 1, 2, 3, 2]})
-#print (df1.mode()['B'][0])
-
-#print( df.mode()['month'][0]) # find the most comman value in month
-
-#print(df.apply(lambda x: sum(x.isnull()), axis=0)) # print the missing values in eche colmn
-
-readStructure ("C:\Users\\nitzan\Desktop\\Structure.txt")
-fillMissingValuesAndDiscretization()
+buildModel("C:\Users\\nitzan\Desktop\\Structure.txt" , m_bins)
